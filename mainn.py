9a9aab63f08aa941f71dd55f879e89d3b7c8d211
prem2103 <prem.sharma@student.suas.ac.in>
2025-06-29 23:16:20 +0530

    Initial commit

---
+++ mainn.py
@@ -0,0 +1,43 @@
+import random
+
+def gameWin(comp, you):
+    if comp == you:
+        return None
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+    elif comp == 'g':
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True
+
+print("Comp Turn: Snake(s) Water(w) or Gun(g)?")
+random_number = random.randint(1, 3)
+if random_number == 1:
+    comp = 's'
+elif random_number == 2:
+    comp = 'w'
+else:
+    comp = 'g'
+
+you = input("Your Turn: Snake(s) Water(w) or Gun(g)? ")
+
+a = gameWin(comp, you)
+
+print(f"Computer chose: {comp}")
+print(f"You chose: {you}")
+
+if a is None:
+    print("The game is a tie!")
+elif a:
+    print("You win!")
+else:
+    print("You lose!")
